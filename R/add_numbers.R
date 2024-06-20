3a6922c6efdb0ee1515a6d7241b1445116f6c819
kashish1928 <khj2003@student.ubc.ca>
2024-06-20 13:31:08 -0700

    Initial commit

---
+++ R/add_numbers.R
@@ -0,0 +1,14 @@
+#' Add two numbers
+#'
+#'This function adds two numbers
+#'
+#' @param x The first number
+#' @param y The second number
+#'
+#' @return The sum of x and y
+#' @export
+#'
+#' @examples add_numbers(2,3)
+add_numbers <- function(x, y) {
+  x + y
+}
